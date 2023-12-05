d2a67c7119d2d20b7dcfff79eab7dd80a509b60f
Hallory <khalpeep@gmail.com>
2023-12-05 15:09:39 +0100

    added link to login/register

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -3,21 +3,26 @@ import Logo from './Logo';
 import SearchBar from './SearchBar';
 import Menu from './Header_menu/Menu';
 import Button from '../../elements/Button';
+import { Link } from 'react-router-dom';
 
 
 const Header = (props) => {
 
+  
+    
 
     return (
-        <header className='flex items-center justify-between space-x-4 py-6'>
+        <header className={`flex items-center justify-between space-x-4 py-6`}>
             <Logo />
             <SearchBar />
             <Menu />
+            <Link to='/login'>
             <Button
                 label='Sign In'
                 onClick={() => { console.log('Click!'); }}
                 variant=''
-            />
+            >
+            </Button></Link>
         </header>
     )
 }
