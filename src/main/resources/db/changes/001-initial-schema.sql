66c4718241d0ff429223ec20b0a93701910a1b95
eugene <yevheniilysikov@gmail.com>
2023-12-11 04:03:08 +0200

    added liquibase migration

---
+++ src/main/resources/db/changes/001-initial-schema.sql
@@ -0,0 +1,7 @@
+create table products (
+    article varchar(255),
+    description varchar(255),
+    id varchar(255) not null,
+    sku varchar(255),
+    primary key (id)
+);
---
+++ src/main/resources/db/changes/002-insert-data-products.sql
@@ -0,0 +1,4 @@
+insert into products (article,description,sku,id) values
+('article1', 'desc1', 'SKU1', '224da616-424a-4cb5-b025-a5f1dffe24ce'),
+('article2', 'desc2', 'SKU2', '2bfb3ef6-0a9e-48e0-bbbb-356843c23621'),
+('article3', 'desc3', 'SKU3', '42a785df-28af-4fbd-a088-983261f485fa');
