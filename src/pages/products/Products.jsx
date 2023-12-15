35d62d5b7ad56eef0682247e8e56d32dea56cc15
Vitalii <vobi188@gmail.com>
2023-12-15 12:03:22 +0200

    updating the filtering and sorting functions (test vers)

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -4,25 +4,35 @@ import ProductCard from '../../components/product/ProductCard';
 import { useSelector } from 'react-redux';
 
 const Products = ({ products }) => {
-
     const filteredProducts = useSelector(state => state.products.filteredProducts);
-    console.log(filteredProducts);
-
     const sortProducts = useSelector(state => state.products.sortProducts);
-    console.log(sortProducts);
-    
-    const displayProducts = sortProducts.length > 0 ? sortProducts : products;
-        
+    const allProducts = useSelector(state => state.products.list);
+
+    // Функція для об'єднання сортування та фільтрації
+    const applySortingAndFiltering = (items) => {
+        // Спробуйте використати сортування, якщо воно присутнє
+        const sortedItems = sortProducts.length > 0 ? sortProducts : items;
+
+        // Повертаємо відфільтровані та відсортовані товари
+        return filteredProducts.length > 0
+            ? sortedItems.filter(product =>
+                filteredProducts.some(filteredProduct => filteredProduct.brand === product.brand)
+            )
+            : sortedItems;
+    };
+
+    // Отримуємо відсортовані та відфільтровані товари
+    const displayedProducts = applySortingAndFiltering(allProducts);
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                {
-                    displayProducts.map((product) => (
-                        <Link to={`/products/${product.id}`} key={product.id}>
-                            <ProductCard product={product} />
-                        </Link>
-                ))}
+            {displayedProducts.map((product) => (
+                <Link to={`/products/${product.id}`} key={product.id}>
+                    <ProductCard product={product} />
+                </Link>
+            ))}
         </div>
-    )
+    );
 };
-        
+
 export default Products;
--- src/redux/slices/productsSlice.js
+++ src/redux/slices/productsSlice.js
@@ -20,38 +20,60 @@ const productsSlice = createSlice({
         error:null
     },
     reducers: {
-        filteredProducts: (state, action) => {
-            try {
-                const filter = state.list.filter((product) => {
-                    return product.brand === action.payload;
-                });
-                state.filteredProducts = filter;
-                console.log('filter', filter);
-                const saveState = JSON.stringify(filter);
-                sessionStorage.setItem('filteredProducts', saveState);
-            } catch (error) {
-                state.error = error.message;
-            }
-        },
-        sortProducts: (state, action) => {
-        try {
+filteredProducts: (state, action) => {
+    try {
+        const brand = action.payload;
+        
+        // Перевіряємо, чи бренд вже є в масиві
+        const isBrandAlreadySelected = state.filteredProducts.some(product => product.brand === brand);
+
+        if (!isBrandAlreadySelected) {
+            // Якщо бренду ще немає в масиві, додаємо всі відповідні товари
+            const brandProducts = state.list.filter(product => product.brand === brand);
+            state.filteredProducts = [...state.filteredProducts, ...brandProducts];
+        } else {
+            // Якщо бренд вже є в масиві, видаляємо всі його товари
+            state.filteredProducts = state.filteredProducts.filter(product => product.brand !== brand);
+        }
+
+        const saveState = JSON.stringify(state.filteredProducts);
+        sessionStorage.setItem('filteredProducts', saveState);
+    } catch (error) {
+        state.error = error.message;
+    }
+},
+sortProducts: (state, action) => {
+    try {
         let sorted = [...state.list];
+        let filteredAndSorted;
 
         switch (action.payload) {
             case 'expensive':
-                state.sortProducts = sorted.sort((a, b) => b.price - a.price);
+                filteredAndSorted = sorted.sort((a, b) => b.price - a.price);
+                state.sortProducts = filteredAndSorted;
                 console.log(state.sortProducts);
                 break;
             case 'cheapest':
-                state.sortProducts = sorted.sort((a, b) => a.price - b.price);
+                filteredAndSorted = sorted.sort((a, b) => a.price - b.price);
+                state.sortProducts = filteredAndSorted;
+                console.log(state.sortProducts);
                 break;
             case 'featured':
-                state.sortProducts = state.list;
+                // Your logic for 'featured' sorting
+                filteredAndSorted = state.list;  // You might want to sort or filter here
+                state.sortProducts = filteredAndSorted;
+                console.log(state.sortProducts);
                 break;
-            // Add other sorting options as needed
             default:
-                // If the action payload doesn't match any defined cases, return the current state
                 return state;
+        }
+
+        // Apply additional filters if present
+        if (state.filteredProducts.length > 0) {
+            // Apply your filtering logic to filteredAndSorted array
+            filteredAndSorted = filteredAndSorted.filter(product =>
+                state.filteredProducts.some(filteredProduct => filteredProduct.brand === product.brand)
+            );
         }
 
         return state;
@@ -61,6 +83,7 @@ const productsSlice = createSlice({
         return state;
     }
 }
+
     },
     extraReducers:(builder)=>{
         builder
