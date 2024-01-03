08cce963ba11f873bab06857f61e373f03a9c427
Hallory <khalpeep@gmail.com>
2024-01-03 16:25:20 +0100

    add details sections

--- src/components/product/ProductsInfoCards.jsx
+++ src/components/product/ProductsInfoCards.jsx
@@ -1,37 +1,56 @@
 import React, { useState } from 'react';
-import ProductDetails from '../../pages/product-details/ProductDetails';
 import MainContent from '../../pages/product-details/main-content/MainContent';
 import ReviewsContent from '../../pages/product-details/reviews-content/ReviewsContent';
 
 const ProductInfoCard = ({ product, products }) => {
-  const [activeTab, setActiveTab] = useState('main');
+    const [activeTab, setActiveTab] = useState('main');
 
-  const renderTabContent = () => {
-    switch (activeTab) {
-      case 'main':
-        return <MainContent product={product} />;
-      case 'description':
-        return <p>{product.description}</p>;
-      case 'reviews': 
-        return <ReviewsContent products={products} />;
-      default:
-        return null;
-    }
-  };
+    const renderTabContent = () => {
+        switch (activeTab) {
+            case 'main':
+                return <MainContent product={product} />;
+            case 'description':
+                return <p>{product.description}</p>;
+            case 'reviews':
+                return <ReviewsContent products={products} />;
+            default:
+                return null;
+        }
+    };
 
-  return (
-    <div className="product-tabs">
-      <div className="tab-buttons flex w-full gap-4 pb-6">
-        <button onClick={() => setActiveTab('main')}>Main</button>
-        <button onClick={() => setActiveTab('description')}>Description</button>
-        <button onClick={() => setActiveTab('reviews')}>Reviews</button>
-        {/* Добавьте дополнительные кнопки для других вкладок */}
-      </div>
-      <div className="tab-content">
-        {renderTabContent()}
-      </div>
-    </div>
-  );
+    return (
+        <div className="product-tabs">
+            <div className="tab-buttons flex p-6 w-full bg-black-200 gap-10 ">
+                <button
+                    className={
+                        activeTab === 'main' ? 'border-b-2 border-primary-400' : ''
+                    }
+                    onClick={() => setActiveTab('main')}
+                >
+                    Main
+                </button>
+                <button
+                    className={
+                        activeTab === 'description'
+                            ? 'border-b-2 border-primary-400'
+                            : ''
+                    }
+                    onClick={() => setActiveTab('description')}
+                >
+                    Description
+                </button>
+                <button
+                    className={
+                        activeTab === 'reviews' ? 'border-b-2 border-primary-400' : ''
+                    }
+                    onClick={() => setActiveTab('reviews')}
+                >
+                    Reviews
+                </button>
+            </div>
+            <div className="tab-content pt-10">{renderTabContent()}</div>
+        </div>
+    );
 };
 
 export default ProductInfoCard;
