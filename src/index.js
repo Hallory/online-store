47d5ced1e2ead4b56d877b85a29183e79af8f7ac
Hallory <khalpeep@gmail.com>
2024-01-11 13:51:38 +0100

    transfer layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -11,9 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter >
-            <Layout>
+       
                 <App />
-            </Layout>
         </BrowserRouter>
     </Provider>
 
