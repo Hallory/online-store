9cfa91bdba5f0959b7dcab464a2571fa4c8f8969
Hallory <khalpeep@gmail.com>
2024-01-11 13:52:22 +0100

    fix paddings und width

--- src/components/image-carousel/ImageCarousel.jsx
+++ src/components/image-carousel/ImageCarousel.jsx
@@ -6,13 +6,13 @@ const ImageCarousel = ({product}) => {
     const images = product.images;
   
         return (
-            <div className="flex select-none">
-                <div className='all-images md:flex flex-col hidden gap-6 max-h-[500px] overflow-y-auto overflow-x-hidden '>
+            <div className="flex select-none w-full m-10">
+                <div className='all-images md:flex flex-col hidden gap-8 max-h-[500px] overflow-y-auto overflow-x-hidden '>
            
                     {images.map((image, index) => (
                         <div
                             key={index}
-                            className={`image ${index === currentIndex ? 'active' : ''} pr-10`}
+                            className={`p-2 image ${index === currentIndex ? 'border-2 border-gray-400 rounded-xl' : 'border-2 border-black-100'} pr-10 `}
                             onClick={() => setCurrentIndex(index)}
                         >
                             <img style={{maxWidth: '150px', maxHeight: '100%'}} src={image} alt=""/>
